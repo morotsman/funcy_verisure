a3c5de0e1f4c8f1fdef2a697c79d334b1eca2488
Niklas Leopold <niklas.leopold@verisure.com>
2020-02-03 16:02:34 +0100

    added example

--- src/main/scala/lecture1/PureExample.scala
+++ src/main/scala/lecture1/PureExample.scala
@@ -29,10 +29,17 @@ object PureExample {
 
 object UnPureAssignment {
 
-  
+  def bmiCalculator(weight: Int, heightInCm: Int): Unit = {
+    val heightInMeters = heightInCm.toDouble / 100
+    weight / (heightInMeters * heightInMeters) match {
+      case bmi@_ if bmi < 18.5 => println(s"Sorry, your underweight, your bmi is: $bmi")
+      case bmi@_ if bmi >= 18.5 && bmi < 25 => println(s"Your normal and your bmi is: $bmi")
+      case bmi@_ if bmi >= 25 => println(s"Your overweight, your bmi is: $bmi")
+    }
+  }
 
   def main(args: Array[String]): Unit = {
-
+    bmiCalculator(70, 178)
   }
 
 }
