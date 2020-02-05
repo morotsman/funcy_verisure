b0167b3b939714ed2be4abdfdb6a99e4dedd2d05
Niklas Leopold <niklas.leopold@verisure.com>
2020-02-05 19:21:36 +0100

    more

--- src/main/scala/excercises/lecture2/Recursive.scala
+++ src/main/scala/excercises/lecture2/Recursive.scala
@@ -2,8 +2,22 @@ package excercises.lecture2
 
 class Recursive {
 
-  def pascalsTriangle(levels: Int): List[List[Int]] = {
-    ???
-  }
+  //F(0) = 0,
+  //F(1) = 1,
+  //F(N) = F(N-1) + F(N-2)
+  def fibonacci(levels: Int): List[Int] = ???
+
+  //n! = n * (n-1) * (n-2) * ... * 2 * 1
+  //0! = 1
+  def factorial(n: Int): Int = ???
+
+  def max(is: List[Int]): Option[Int] = ???
+
+  //    1
+  //   1 1
+  //  1 2 1
+  // 1 3 3 1
+  //1 4 6 4 1
+  def pascalsTriangle(levels: Int): List[List[Int]] = ???
 
 }
